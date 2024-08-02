9c6c9abf3abd16c7ea47ce85f0bfafeef0d2a5c6
EnzoKsol <132067957+biboSguy@users.noreply.github.com>
2024-08-02 11:16:32 -0300

    Add files via upload

---
+++ exemplos.js
@@ -0,0 +1,43 @@
+let primeironome = "sonic"
+let ultimonome = "preto"
+let nomecompleto = primeironome+" "+ultimonome 
+console.log(nomecompleto)
+
+
+let n1 = 50
+let n2 = 20
+let soma = n1+n2
+console.log(soma)
+let sub = n1-n1
+console.log(sub)
+let mult = n1*n2
+console.log(mult)
+let div = n1/n2
+console.log(div)
+
+
+let estachovendo = true
+if(estachovendo){
+    console.log("esta chovendo")
+}else{
+    console.log("nao esta chovendo")
+}
+
+
+let resposta = null
+let nota
+console.log(resposta, nota)
+
+let idunico = Symbol()
+let produto ={
+    [idunico]: 123,
+    nome: "camiseta"
+}
+console.log(produto[idunico], produto.nome)
+
+
+let numerogrande = BigInt("92409821482194821948219849218")
+let numero = 92409821482194821948219849218
+console.log(numerogrande, numero)
+
+
